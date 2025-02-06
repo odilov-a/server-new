9a80213de18b3c867741ae0ea58f42a4fe8438d2
Akbar Odilov <akbarjonodilov01@gmail.com>
2025-02-06 11:33:18 +0500

    fix(email): update environment variables for Gmail integration in student registration

--- src/controllers/student.controller.js
+++ src/controllers/student.controller.js
@@ -131,8 +131,8 @@ exports.registerStudent = async (req, res) => {
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS,
       },
     });
     const mailOptions = {
